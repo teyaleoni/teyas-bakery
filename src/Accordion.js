b70fc4d5b3d0bb279e60303e48bdfc98126d3e49
Teya Urena <teyaleoni@gmail.com>
2021-02-18 16:10:56 -0800

    add stuff

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,31 +1,39 @@
 import React, { useState } from "react";
 import "./Accordion.css";
 
+function ShapeContent() {
+  return <p>Shape!</p>;
+}
+const Dummy = () => <div>This is just a dummy content component</div>;
+
 function Accordion() {
   const [activeIndex, setActiveIndex] = useState(0);
 
   const categories = [
-    { title: "Shape" },
-    { title: "Size" },
-    { title: "Tiers" },
-    { title: "Sponge" },
-    { title: "Fillings" },
-    { title: "Buttercream" },
-    { title: "Cake Board" },
-    { title: "Decorations" },
-    { title: "Party Add-ons" },
+    { title: "Shape", Content: ShapeContent },
+    { title: "Size", Content: Dummy },
+    { title: "Tiers", Content: Dummy },
+    { title: "Flavor", Content: Dummy },
+    { title: "Buttercream", Content: Dummy },
+    { title: "Fillings", Content: Dummy },
+    { title: "Cake Decorations", Content: Dummy },
+    { title: "Cake Boards/Cake Boxes", Content: Dummy },
+    { title: "Party Add-ons", Content: Dummy },
   ];
 
   return (
-    <ul className={"Accordion"}>
-      {categories.map((category, index) => (
+    <ul className={"accordion"}>
+      {categories.map(({ title, Content }, index) => (
         <li
           className={`category ${index === activeIndex ? "active" : ""}`}
           onClick={() => {
             setActiveIndex(index);
           }}
         >
-          {category.title}
+          {title}
+          <div className={index === activeIndex ? "show" : "hide"}>
+            <Content />
+          </div>
         </li>
       ))}
     </ul>
