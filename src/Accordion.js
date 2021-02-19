f5198116db16b7812a4a80cb852a8e20390e5683
Teya Urena <teyaleoni@gmail.com>
2021-02-18 20:05:20 -0800

    Add stuff

--- src/Accordion.js
+++ src/Accordion.js
@@ -2,19 +2,66 @@ import React, { useState } from "react";
 import "./Accordion.css";
 
 function ShapeContent() {
-  return <p>Shape!</p>;
+  return (
+    <div>
+      <button>Sheet</button>
+      <button>Round</button>
+      <button>Square</button>
+    </div>
+  );
 }
-const Dummy = () => <div>This is just a dummy content component</div>;
+
+function SizeContent() {
+  return (
+    <div>
+      <div>
+        SHEET
+        <ul>
+          <li>2" MINI</li>
+          <li>9" x 13"</li>
+          <li>10" x 15"</li>
+        </ul>
+      </div>
+      <div>
+        ROUND
+        <ul>
+          <li>2" MINI</li>
+          <li>6"</li>
+          <li>8"</li>
+          <li>10"</li>
+        </ul>
+      </div>
+      <div>
+        SQUARE
+        <ul>
+          <li>8"</li>
+          <li>9"</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+function TiersContent() {
+  return (
+    <div>
+      <ul>
+        <li>YES</li>
+        <li>NO</li>
+      </ul>
+    </div>
+  );
+}
+const Dummy = () => <div>Hello</div>;
 
 function Accordion() {
   const [activeIndex, setActiveIndex] = useState(0);
 
   const categories = [
     { title: "Shape", Content: ShapeContent },
-    { title: "Size", Content: Dummy },
-    { title: "Tiers", Content: Dummy },
+    { title: "Size", Content: SizeContent },
+    { title: "Tiers", Content: TiersContent },
     { title: "Flavor", Content: Dummy },
-    { title: "Buttercream", Content: Dummy },
     { title: "Fillings", Content: Dummy },
     { title: "Cake Decorations", Content: Dummy },
     { title: "Cake Boards/Cake Boxes", Content: Dummy },
@@ -29,8 +76,9 @@ function Accordion() {
           onClick={() => {
             setActiveIndex(index);
           }}
+          key={index}
         >
-          {title}
+          <div className="title">{title}</div>
           <div className={index === activeIndex ? "show" : "hide"}>
             <Content />
           </div>
